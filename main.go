27312f18ede8e623cb07d6db12a93b0a0aa63205
aburdulescu <andrei.burdulescu@pm.me>
2021-04-16 00:21:48 +0100

    Update main.go
    Keep main simple, do everything in run func.

--- main.go
+++ main.go
@@ -11,38 +11,52 @@ import (
 
 const version = "0.1"
 
-func handleError(err error) {
-	fmt.Fprintln(os.Stderr, "error:", err)
-	os.Exit(1)
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
 }
 
-func main() {
+func run() error {
 	var metadataFile string
-	flag.StringVar(&metadataFile, "f", "mkdeb.yaml", "path to mkdeb.yaml file")
 	var printVersion bool
+
+	flag.StringVar(&metadataFile, "f", "mkdeb.yaml", "path to mkdeb.yaml var")
 	flag.BoolVar(&printVersion, "v", false, "print version")
+
 	flag.Parse()
+
 	if printVersion {
 		fmt.Println(version)
-		return
+		return nil
 	}
+
 	args := flag.Args()
+
 	if len(args) != 1 {
-		handleError(errors.New("path to output directory wasn't provided"))
+		return errors.New("path to output directory wasn't provided")
 	}
+
 	outputDir := args[0]
+
 	f, err := os.Open(metadataFile)
 	if err != nil {
-		handleError(err)
+		return err
 	}
+
 	var m Metadata
 	if err := yaml.NewDecoder(f).Decode(&m); err != nil {
-		handleError(err)
+		return err
 	}
+
 	if err := m.Validate(); err != nil {
-		handleError(err)
+		return err
 	}
+
 	if err := m.Generate(outputDir); err != nil {
-		handleError(err)
+		return err
 	}
+
+	return nil
 }
